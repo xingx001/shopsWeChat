469857caed578af6812df0ae13b097b1c63e1bb0
linhe <linhe@dtstack.com>
2020-07-02 19:48:44 +0800

    feat(coupons): 优惠券发布接口字段填充

--- src/pages/coupons/editCoupons/index.tsx
+++ src/pages/coupons/editCoupons/index.tsx
@@ -1,16 +1,16 @@
 import { ComponentClass } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Input, Text, Textarea, Picker, Image } from '@tarojs/components';
-import { AtModal } from 'taro-ui';
 import { API } from '@/apis';
 import './style.scss'
+import RangeDatePicker from '@/components/rangeDatePicker';
 
 interface IProps {
 }
 const initState = {
   authsInfo: Taro.getStorageSync('authsInfo') || {},
   isOpened: false,
-  radioType: 1,
+  radioType: 0,
   Id:0,//优惠券Id(新增为0 修改为优惠券ID)
   scname:'',//优惠券名称
   ctype:1,// 优惠券类型 （1=折扣券，2=代金券）
@@ -64,10 +64,12 @@ class EditCoupons extends Component<IProps, IState> {
       radioType: e
     })
   }
-  onChangePicker = () => {
-
+  onChangePicker = (e) => {
+    console.log(e);
   }
-  handleChange = () => { }
+  onChangeInput = (e,type) => {
+
+   }
   onHandleAdd = () => {
     const { authsInfo,Id,	scname,ctype,	limit,cinfo,cstate,paynum,bnum,btime,etime,issue } = this.state;
     API.savePOSShopCardManage({
@@ -85,6 +87,16 @@ class EditCoupons extends Component<IProps, IState> {
       })
 
   }
+  onSelectDate = () => {
+    this.setState({
+      isOpened:true
+    })
+  }
+  onCancelSelectDate = () => {
+    this.setState({
+      isOpened:false
+    })
+  }
   render() {
     const { isOpened, radioType,scname,ctype,	limit,cinfo,cstate,paynum,bnum,btime,etime,issue } = this.state;
     return (
@@ -93,7 +105,7 @@ class EditCoupons extends Component<IProps, IState> {
         <View className="goods_info">
           <View className="info_li">
             <View className="label">优惠券名称</View>
-            <Input type='text' className="input" value={scname} placeholder='如：8.8折券，最多15个字' placeholderClass="placeholderClass" maxLength={15} />
+            <Input type='text' className="input" value={scname} onInput={(e)=>this.onChangeInput(e,'scname')} placeholder='如：8.8折券，最多15个字' placeholderClass="placeholderClass" maxLength={15} />
           </View>
           <View className="info_li_allow">
             <View className="label">优惠券类型</View>
@@ -107,18 +119,18 @@ class EditCoupons extends Component<IProps, IState> {
             <View className="label">使用条件</View>
             <View className="allow conditions_box">
               <View className="conditions_li" onClick={() => this.onChangeRadio(1)}>
-                <Image src={radioType == 1 ? require('@/assets/images/icon/select.png') : require('@/assets/images/icon/noselect.png')} className="icon" />
+                <Image src={radioType == 0 ? require('@/assets/images/icon/select.png') : require('@/assets/images/icon/noselect.png')} className="icon" />
                 <Text className="desc">无使用门槛</Text>
               </View>
               <View className="conditions_li" onClick={() => this.onChangeRadio(2)}>
-                <Image src={radioType == 2 ? require('@/assets/images/icon/select.png') : require('@/assets/images/icon/noselect.png')} className="icon" />
-                <View className="desc desc_input">订单满<Input type='text' className="input_desc" placeholder='' placeholderClass="placeholderClass" maxLength={15} />元</View>
+                <Image src={radioType == 1 ? require('@/assets/images/icon/select.png') : require('@/assets/images/icon/noselect.png')} className="icon" />
+                <View className="desc desc_input">订单满<Input type='number' value={`${limit}`} onInput={(e)=>this.onChangeInput(e,'limit')} className="input_desc" placeholder='' placeholderClass="placeholderClass" maxLength={15} />元</View>
               </View>
             </View>
           </View>
           <View className="info_li">
             <View className="label">优惠券描述</View>
-            <Textarea className="input_area" autoHeight placeholder='请输入' placeholderClass="placeholderClass" />
+            <Textarea className="input_area" autoHeight value={cinfo} onInput={(e)=>this.onChangeInput(e,'cinfo')} placeholder='请输入' placeholderClass="placeholderClass" />
           </View>
         </View>
         <View className="title">上架信息</View>
@@ -126,47 +138,37 @@ class EditCoupons extends Component<IProps, IState> {
           <View className="info_li_allow">
             <View className="label">立即上架</View>
             <View className="allow">
-              <Picker value={0} mode='selector' range={['上架', '下架']} onChange={this.onChangePicker}>
-                上架<Text className="at-icon at-icon-chevron-right store_right" ></Text>
+              <Picker value={0} mode='selector' rangeKey="text" range={[{value:'1',text:'正常'},{value:'2',text:'下架'},{value:'3',text:'售罄'}]} onChange={this.onChangePicker}>
+                <Text className="placeholderClass">请选择</Text><Text className="at-icon at-icon-chevron-right store_right" ></Text>
               </Picker>
             </View>
           </View>
           <View className="info_li_allow">
             <View className="label">售价</View>
-            <Input type='number' className="input" placeholder='请输入金额' placeholderClass="placeholderClass" maxLength={15} />
+            <Input type='number' value={`${paynum}`} onInput={(e)=>this.onChangeInput(e,'paynum')} className="input" placeholder='请输入金额' placeholderClass="placeholderClass" maxLength={15} />
             <View className="allow">元</View>
           </View>
           <View className="info_li_allow">
             <View className="label">发放总量</View>
-            <Input type='number' className="input" placeholder='请输入整数' placeholderClass="placeholderClass" maxLength={15} />
+            <Input type='number' value={`${bnum}`} onInput={(e)=>this.onChangeInput(e,'bnum')} className="input" placeholder='请输入整数' placeholderClass="placeholderClass" maxLength={15} />
             <View className="allow">张</View>
           </View>
           <View className="info_li_allow">
             <View className="label">使用时间段</View>
-            <View className="allow">
-              <Picker mode='date' value={''} onChange={this.onChangePicker}>
-                请选择<Text className="at-icon at-icon-chevron-right store_right" ></Text>
-              </Picker>
+            <View className="allow" onClick={this.onSelectDate}>
+              <Text className="placeholderClass">请选择</Text><Text className="at-icon at-icon-chevron-right store_right" ></Text>
+              <RangeDatePicker isOpened={isOpened} type="date" BiginTiem={btime} EndTiem={etime} onCancel={this.onCancelSelectDate} onChange={this.onChangePicker}/>
             </View>
           </View>
           <View className="info_li_allow">
             <View className="label">发行至页面</View>
             <View className="allow">
-              <Picker value={0} mode='selector' range={['上架', '下架']} onChange={this.onChangePicker}>
-                请选择<Text className="at-icon at-icon-chevron-right store_right" ></Text>
+              <Picker value={0} mode='selector' rangeKey="text" range={[{value:'1',text:'首页广告栏'},{value:'2',text:'优惠券栏目'},{value:'3',text:'店内会员'}]} onChange={this.onChangePicker}>
+                <Text className="placeholderClass">请选择</Text><Text className="at-icon at-icon-chevron-right store_right" ></Text>
               </Picker>
             </View>
           </View>
         </View>
-        <AtModal
-          isOpened={isOpened}
-          cancelText='取消'
-          confirmText='确认'
-          onClose={this.handleCancel}
-          onCancel={this.handleCancel}
-          onConfirm={this.handleConfirm}
-          content='确定删除该商品吗？'
-        />
         <View className="fix_bottom_btn" onClick={this.onHandleAdd}>立即创建</View>
       </View>
     )
