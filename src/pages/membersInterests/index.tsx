23ca29dec4395024b9e8178f10d66ee6a6549f89
linhe <linhe@dtstack.com>
2020-07-05 14:06:42 +0800

    feat(echart): 新增会员权益及图表优化

--- src/pages/membersInterests/index.tsx
+++ src/pages/membersInterests/index.tsx
@@ -2,22 +2,34 @@ import { ComponentClass } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Text, Input, Textarea } from '@tarojs/components'
 import './style.scss'
+import { API } from '@/apis'
 
 interface IProps {
+
 }
 interface IState {
+  authsInfo:any,
+  title:'',
+  svcList:any[]
 }
 
 class Index extends Component<IProps, IState> {
   state: IState = {
-
+    authsInfo: Taro.getStorageSync('authsInfo') || {},
+    title:'',
+    svcList:[{
+      title:'',
+      ctype:'',
+      num:1,
+      ccontent:''
+    }]
   }
   config: Config = {
     navigationBarTitleText: '会员权益'
   }
 
   handleChange = value => {
-    this.setState({ value })
+
   }
 
   componentWillReceiveProps(nextProps) {
@@ -31,45 +43,97 @@ class Index extends Component<IProps, IState> {
   componentDidShow() { }
 
   componentDidHide() { }
+  onChangeInput = (e,type='',index=0) => {
+    const value = e.target.value;
+    const { svcList } = this.state;
+    switch(type){
+      case 'title':
+        this.setState({
+          title:value
+        });
+        break;
+      default:
+          svcList[index] = Object.assign({},svcList[index],{[type]:value})
+          this.setState({
+            svcList
+          });
+          break;
+    }
+  }
+  onDelete = (index) => {
+    const { svcList } = this.state;
+    svcList.splice(index,1);
+    this.setState({
+      svcList
+    })
+  }
+  onHandleAdd = () => {
+    const { svcList } = this.state;
+    this.setState({
+      svcList:[...svcList,{
+        title:'',
+        ctype:'',
+        num:1,
+        ccontent:''
+      }]
+    })
+  }
+  onHandleSave = () => {
+    const { authsInfo,svcList,title } = this.state;
+    const List = svcList.map(item=>Object.assign({},item,{title}));
+    const params = {
+      ...authsInfo,
+      svcList:List
+    }
+    API.saveShopVIPConfigManage(params).then(res=>{
+      console.log(res);
+    })
+
+  }
   render() {
+    const { svcList,title } = this.state;
     return (
       <View className='interests-wrap'>
         <View className="interests-ul ">
           <View className="interests-li">
             <Text className="interests-tit">标题</Text>
             <View className="interests-input">
-              <Input type='number' className="input" placeholder='不超过15个字' placeholderClass="placeholderClass" maxLength={15} />
+              <Input type='number' value={title} onInput={(e)=>this.onChangeInput(e,'title')} className="input" placeholder='不超过15个字' placeholderClass="placeholderClass" maxLength={15} />
             </View>
           </View>
         </View>
-        <View className="interests-ul content_box">
+        {
+          svcList.map((item,index)=>(
+           <View className="interests-ul content_box" key={'li'+index}>
           <View className="interests-li boder_bottom">
             <Text className="interests-tit">类型</Text>
             <View className="interests-input">
-              <Input type='number' className="input" placeholder='请输入类型' placeholderClass="placeholderClass" maxLength={15} />
+              <Input type='number' value={item.ctype} onInput={(e)=>this.onChangeInput(e,'ctype',index)}  className="input" placeholder='请输入类型' placeholderClass="placeholderClass" maxLength={15} />
             </View>
           </View>
           <View className="interests-li boder_bottom">
             <Text className="interests-tit">编号</Text>
             <View className="interests-input">
-              <Input type='number' className="input" placeholder='请输入编号' placeholderClass="placeholderClass" maxLength={15} />
+              <Input type='number' value={item.num} onInput={(e)=>this.onChangeInput(e,'num',index)}  className="input" placeholder='请输入编号' placeholderClass="placeholderClass" maxLength={15} />
             </View>
           </View>
           <View className=" interests-content">
             <Text className="interests-tit">内容</Text>
             <View className="interests-input">
-              <Textarea placeholder='请输入会员权益具体内容' placeholderClass="placeholderClass" />
+              <Textarea value={item.ccontent} onInput={(e)=>this.onChangeInput(e,'ccontent',index)}  placeholder='请输入会员权益具体内容' placeholderClass="placeholderClass" />
             </View>
           </View>
+          <View><Text onClick={()=>this.onDelete(index)}>删除</Text></View>
         </View>
-
+          ))
+        }
 
 
       <View className="btn_wrap">
-        <View className="add_btn btn">
+        <View className="add_btn btn" onClick={this.onHandleAdd}>
           新增
         </View>
-        <View className="save_btn btn">
+        <View className="save_btn btn" onClick={this.onHandleSave}>
           提交
         </View>
       </View>
--- src/pages/shopManage/index/index.tsx
+++ src/pages/shopManage/index/index.tsx
@@ -9,20 +9,20 @@ interface IProps {
 }
 interface IState {
   current: number,
-  activeTabKey:string|number,
-  isOpened:boolean,
-  authsInfo:any,
-  proList:any,
-  proTypeList:any
+  activeTabKey: string | number,
+  isOpened: boolean,
+  authsInfo: any,
+  proList: any,
+  proTypeList: any
 }
 class Index extends Component<IProps, IState> {
   state: IState = {
     authsInfo: Taro.getStorageSync('authsInfo') || {},
     current: 0,
-    activeTabKey:'推荐',
-    isOpened:false,
-    proList:[],
-    proTypeList:[]
+    activeTabKey: '推荐',
+    isOpened: false,
+    proList: [],
+    proTypeList: []
   }
   config: Config = {
     navigationBarTitleText: '商品管理',
@@ -43,11 +43,11 @@ class Index extends Component<IProps, IState> {
 
   }
   getShopDataReq() {
-    const { authsInfo,activeTabKey } = this.state;
-    API.getPOSProManagePage({...authsInfo,ptype:activeTabKey}).then(res => {
+    const { authsInfo, activeTabKey } = this.state;
+    API.getPOSProManagePage({ ...authsInfo, ptype: activeTabKey }).then(res => {
       const { code, data } = res;
       if (code === '0') {
-        const {proList,proTypeList} = data;
+        const { proList, proTypeList } = data;
         this.setState({
           proList,
           proTypeList
@@ -67,22 +67,24 @@ class Index extends Component<IProps, IState> {
   }
   onChangeTabs = (value) => {
     this.setState({
-      activeTabKey:value
-    },this.getShopDataReq)
+      activeTabKey: value
+    }, this.getShopDataReq)
   }
-  onDeleteShop = (pid) => {
+  onDeleteShop = (e, pid) => {
+    e.stopPropagation();
+
     Taro.showModal({
-      title:'',
-      content:'确定删除该商品吗？',
-      cancelText:'取消',
-      confirmText:'确认',
-      confirmColor:'#F5A623',
-      success:(res)=>{
-        if(res.confirm){
-          const {authsInfo} = this.state;
-          API.deletePOSDelProduct({...authsInfo,pid}).then(res=>{
-            const { code } =res;
-            if(code==0){
+      title: '',
+      content: '确定删除该商品吗？',
+      cancelText: '取消',
+      confirmText: '确认',
+      confirmColor: '#F5A623',
+      success: (res) => {
+        if (res.confirm) {
+          const { authsInfo } = this.state;
+          API.deletePOSDelProduct({ ...authsInfo, pid }).then(res => {
+            const { code } = res;
+            if (code == 0) {
               this.getShopDataReq();
               Taro.showToast({
                 'title': '删除成功',
@@ -94,40 +96,40 @@ class Index extends Component<IProps, IState> {
       }
     })
   }
-  onHandleAdd = () => {
+  onHandleAdd = (type = 0, id = '') => {
     Taro.navigateTo({
-      url: "/pages/shopManage/editShop/index?type=0&id="
+      url: "/pages/shopManage/editShop/index?type=" + type + "&id=" + id
     })
   }
   render() {
-    const {activeTabKey ,proList,proTypeList} = this.state;
-    const tabsData = proTypeList.map((item,index) => {
+    const { activeTabKey, proList, proTypeList } = this.state;
+    const tabsData = proTypeList.map((item, index) => {
       return { text: item.ProTypeName, value: item.ProTypeName }
     })
     return (
       <View className='shop-index'>
-        <View className="goods_tabs"><Tags value={activeTabKey} data={tabsData} onChange={this.onChangeTabs}/></View>
+        <View className="goods_tabs"><Tags value={activeTabKey} data={tabsData} onChange={this.onChangeTabs} /></View>
         <View className="goods_info">
           <View className="goods_ul">
-          {
-            proList.map(item=>
-            <View className="goods_li" key={item.Id}>
-              <Image src={item.Pru_Img} className="good_img">
-                <View className="delect-btn" onClick={()=>this.onDeleteShop(item.Id)}>
-                  <AtIcon value='trash' size='18' color='rgba(255, 255, 255, 1)' className="icon-del"></AtIcon>
+            {
+              proList.map(item =>
+                <View className="goods_li" key={item.Id}>
+                  <Image src={item.Pru_Img} className="good_img">
+                    <View className="delect-btn" onClick={(e) => this.onDeleteShop(e, item.Id)}>
+                      <AtIcon value='trash' size='18' color='rgba(255, 255, 255, 1)' className="icon-del"></AtIcon>
+                    </View>
+                  </Image>
+                  <View className="good_content" onClick={() => this.onHandleAdd(1, item.Id)}>
+                    <View className="good_name">{item.Pru_Name}</View>
+                    <View className="good_desc">{item.EzInfo}</View>
+                    <View className="good_price">¥{item.Pru_Money}</View>
+                  </View>
                 </View>
-              </Image>
-              <View className="good_content">
-            <View className="good_name">{item.Pru_Name}</View>
-            <View className="good_desc">{item.EzInfo}</View>
-            <View className="good_price">¥{item.Pru_Money}</View>
-              </View>
-            </View>
-            )
-          }
+              )
+            }
           </View>
         </View>
-        <View className="fix_bottom_btn" onClick={this.onHandleAdd}>新增商品</View>
+        <View className="fix_bottom_btn" onClick={() => this.onHandleAdd(0)}>新增商品</View>
       </View>
     )
   }
